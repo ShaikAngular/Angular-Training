3fee0af6e626ebbdf4c2e87a8dd8fa4ce2fa88b8
shaik <indivarangular@gmail.com>
2023-01-09 23:47:42 +0530

    Working with RxJs operators, WIP

--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { from, fromEvent } from 'rxjs';
 import { of } from 'rxjs/internal/observable/of';
-import { first, map, tap } from 'rxjs/operators';
+import { filter, first, map, tap, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -9,26 +10,81 @@ import { first, map, tap } from 'rxjs/operators';
 })
 export class RxjsComponent implements OnInit {
 
-  a: number[] = [1, 2, 3, 4, 5];
+  a: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   a$;
 
+  filtResult: any;
+
   demo$ = of(2, 3, 4, 5, 6);
+  mapArray = [];
+  filterArr = [];
+  debouncer = [];
   constructor() { }
 
   ngOnInit(): void {
-   
-    this.a$ = of(this.a)
-   
+    this.debounceTimer();
+
+    this.a$ = from(this.a) //better use from operator while working with arrays as from checks every element
+
+
+
+    //  const y = this.a$.pipe (
+    //     //tap(res=>console.log(res)),
+    //     filter((num:any) => num%2===0)
+    //     //filter((num:any) => this.even(num))
+    //   )
+
+
+
+  }
+
+  rxjsMap() {
+    //map operator
     const x: any = this.a$.pipe(
-      tap(res=>console.log(res)),
-      map((num: any) => num.map(a => a * a))
+      tap(res => console.log(res)),
+      map((a: any) => a * a)
     ).subscribe(
-      r => console.log(r)
+      r => {
+        console.log(r)
+        this.mapArray = [...this.mapArray, r];
+      }
     )
-	
+    console.log('map array', this.mapArray)
+  }
 
+  rxjsFilter() {
+    this.filtResult = this.a$.pipe(
+      //tap(res=>console.log(res)),
+      filter((num: any) => num % 2 === 0)
+      //filter((num:any) => this.even(num))
+    ).subscribe(val => {
+      this.filterArr = [...this.filterArr, val]
+      console.log(`Even number: ${val}`)
 
+    });
+    console.log('arr', this.filterArr)
+  }
 
+  debounceTimer() {
+    // elem ref
+    const searchBox = document.getElementById('search');
+
+    // streams
+    const keyup$ = fromEvent(searchBox, 'keyup');
+
+    // wait .5s between keyups to emit current value
+    keyup$.pipe(
+      map((i: any) => i.currentTarget.value),
+      debounceTime(500)
+    )
+      .subscribe(res=>{
+        console.log('debounce example-->', res);
+        this.debouncer = [...this.debouncer,res];
+      });
+  }
+
+  even(num) {
+    return num % 2 === 0;
   }
 
 }
